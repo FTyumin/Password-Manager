147b0acfe6bb36d634bb15b5cd54090b825f1ed1
FTyumin <71492298+FTyumin@users.noreply.github.com>
2023-09-28 18:43:06 +0300

    Edits

--- password_manager/models.py
+++ password_manager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Password(models.Model):
+    title = models.CharField(max_length=200)
+    username = models.CharField("auth.User",
+                                on_delete=models.CASCADE,)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
