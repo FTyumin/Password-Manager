9682ca1b056c1c8f8c5e5741463248b6307b8a2d
FTyumin <71492298+FTyumin@users.noreply.github.com>
2023-09-29 18:22:26 +0300

    Edits

--- password_manager/views.py
+++ password_manager/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
+from .models import Password
 
 # Create your views here.
 
 class HomePageView(TemplateView):
     template_name = "home.html"
 
+class NewPasswordView(CreateView):
+    model = Password
+    template_name = "new_password.html"
+    fields = ["title", "username", "body"]
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+
+class DeletePassword(DeleteView):
+    model = Password
+    template_name = "delete_password.html"
+    success_url = "/"
+
+    
