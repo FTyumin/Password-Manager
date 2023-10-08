38a458ad3cce192e0dcaaa54f249278c879f30df
FTyumin <71492298+FTyumin@users.noreply.github.com>
2023-10-08 18:50:39 +0300

    Fixed error

--- password_manager/urls.py
+++ password_manager/urls.py
@@ -4,8 +4,8 @@ from .views import HomePageView, PasswordCreateView, PasswordListView, PasswordD
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path("new/",PasswordCreateView.as_view(), name="password_new" ),
-    path("list/", PasswordListView.as_view(), name="password_list"),
+    path("password/new/",PasswordCreateView.as_view(), name="password_new" ),
+    path("password/list/", PasswordListView.as_view(), name="password_list"),
     path("delete/", PasswordDeleteView.as_view(), name="password_delete"),
     path("password/<int:pk>/edit", PasswordUpdateView.as_view(), name="password_edit"),
 ]
