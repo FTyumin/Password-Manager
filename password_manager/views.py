da4851e07efaf6df9df698aee6bca5c7bcf22eb6
FTyumin <71492298+FTyumin@users.noreply.github.com>
2023-10-08 18:03:51 +0300

    Fixed small problem

--- password_manager/views.py
+++ password_manager/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView, ListView
+from django.views.generic import (
+    TemplateView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    ListView,
+)
 from .models import Password
 
-# Create your views here.
+
 
 class HomePageView(TemplateView):
+    model = Password
     template_name = "home.html"
+
 
 class PasswordCreateView(CreateView):
     model = Password
@@ -16,25 +24,25 @@ class PasswordCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-    
+
+
 class PasswordUpdateView(UpdateView):
     model = Password
     template_name = "password_edit.html"
     fields = ["title", "password"]
     success_url = "/"
-    
+
 
 class PasswordDeleteView(DeleteView):
     model = Password
     template_name = "delete_password.html"
     success_url = "/"
 
-    
+
 class PasswordListView(ListView):
     model = Password
     template_name = "password_list.html"
     context_object_name = "passwords"
 
-    def password_list_view(self,request,*args,**kwargs):
-        password_list = Password.objects.all()
-        return render(request, self.template_name, {'passwords': password_list})
+    def get_queryset(self):
+        return Password.objects.all()
