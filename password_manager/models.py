409564af18b99c5be3ca87aa89479be447481e19
FTyumin <71492298+FTyumin@users.noreply.github.com>
2023-11-05 16:25:55 +0200

    Update password model and templates

--- password_manager/models.py
+++ password_manager/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 class Password(models.Model):
     title = models.CharField(max_length=200)
-    password = models.TextField()
+    text = models.TextField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='user_passwords'
+    )
 
     def __str__(self):
         return self.title
